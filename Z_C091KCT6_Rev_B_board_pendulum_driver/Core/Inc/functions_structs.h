e42c6f3a16c085604c2fca5eb8909cacbb45006c
Andrew Chisholm <chisatholm@yahoo.co.uk>
2025-08-24 16:42:40 +0100

    functioning period and amplitude regulation, next up is pinging

--- Z_C091KCT6_Rev_B_board_pendulum_driver/Core/Inc/functions_structs.h
+++ Z_C091KCT6_Rev_B_board_pendulum_driver/Core/Inc/functions_structs.h
@@ -10,58 +10,56 @@
 
 #include <stdbool.h>
 
-
 struct time_tracker_t // excludes all magnetic measurement times, these are held in the array pos_array[][]
-  {
-  unsigned long long loop_time_current; // uint16_t
-  unsigned long long loop_time_previous;
+{
+	unsigned long long loop_time_current; // uint16_t
+	unsigned long long loop_time_previous;
 
-  unsigned long long pgzc_time_current;
-  unsigned long long pgzc_time_previous;
+	unsigned long long pgzc_time_current;
+	unsigned long long pgzc_time_previous;
 
-  unsigned long long ngzc_time_current;
-  unsigned long long ngzc_time_previous;
+	unsigned long long ngzc_time_current;
+	unsigned long long ngzc_time_previous;
 
-  unsigned long long pgzc_time_last_desired;
-  unsigned long long pgzc_time_next_desired;
+	unsigned long long pgzc_time_last_desired;
+	unsigned long long pgzc_time_next_desired;
 
-  long measurement_interval; // milliseconds between position measurements
-  long motor_update_interval; // milliseconds between motor instructions TODO is this variable AND measurement_interval needed?
+	long measurement_interval; // milliseconds between position measurements
+	long motor_update_interval; // milliseconds between motor instructions TODO is this variable AND measurement_interval needed?
 
-  long meta_period_start_time;
-  };
+	long meta_period_start_time;
+};
 
-struct pendulum_t
-  {
-    long neutral_position;				// value returned by encoder when motor is turned off
-    int  offset;						// empirically found - positional motor offset
-    long pos_reading_provisional_max; 	// hold the max position reading as it is collected in this variable
-    long pos_reading_max;             	// receive the the max reading at ngzc into this variable
-    long pos_reading_provisional_min; 	// hold the min position reading as it is collected in this variable
-    long pos_reading_min;             	// receive the min reading at pgzc into this variable
-    int  current_vector[3];           	// not used
-    int  pendulum_number;				// number 0 - 7, 0 for controller, 1-7 for peripherals
-    int  periods_in_meta_period;		// the number of periods of the slowest pendulum in one meta period
-    long meta_period;					// length of a meta period in milliseconds
-    long desired_period;				// the period of the pendulum required to form the pattern, milliseconds
-    long desired_period_us;				// the period of the pendulum required to form the pattern, microseconds
-    long period;						// the actual period of the pendulum
-    int  time_tolerance;				// PGZC acceptable error - used for category five swing, in which period and amplitude were OK / required no change
-    long period_tolerance;				// period acceptalbe error
-    long period_error;					// period actual error
-    long pgzc_error;					// PGZC actual error
-    long cumulative_pgzc_error;
-    long desired_amplitude;				// desired amplitude of swing
-    long amplitude;						// actual amplitude of swing
-    int  amplitude_tolerance;			// acceptable error in amplitude
-    long pos_array[11][2];				// circular array to hold position and time data, see below this struct definition for further detail
-    bool pgzc_flag;
-    bool ngzc_flag;
-    unsigned long long pgzc_time;						// Positive Going Zero Crossing
-    unsigned long long ngzc_time;						// Negative G     Z    C
-    bool early;							// if true, pendulum arrived at last pgzc early
+struct pendulum_t {
+	long neutral_position;// value returned by encoder when motor is turned off
+	int offset;					// empirically found - positional motor offset
+	long pos_reading_provisional_max; // hold the max position reading as it is collected in this variable
+	long pos_reading_max; // receive the the max reading at ngzc into this variable
+	long pos_reading_provisional_min; // hold the min position reading as it is collected in this variable
+	long pos_reading_min;  // receive the min reading at pgzc into this variable
+	int current_vector[3];           	// not used
+	int pendulum_number;// number 0 - 7, 0 for controller, 1-7 for peripherals
+	int periods_in_meta_period;	// the number of periods of the slowest pendulum in one meta period
+	long meta_period;				// length of a meta period in milliseconds
+	long desired_period;// the period of the pendulum required to form the pattern, milliseconds
+	long desired_period_us;	// the period of the pendulum required to form the pattern, microseconds
+	long period;						// the actual period of the pendulum
+	int time_tolerance;	// PGZC acceptable error - used for category five swing, in which period and amplitude were OK / required no change
+	long period_tolerance;				// period acceptalbe error
+	long period_error;					// period actual error
+	long pgzc_error;					// PGZC actual error
+	long cumulative_pgzc_error;
+	long desired_amplitude;				// desired amplitude of swing
+	long amplitude;						// actual amplitude of swing
+	int amplitude_tolerance;			// acceptable error in amplitude
+	long pos_array[11][2];// circular array to hold position and time data, see below this struct definition for further detail
+	bool pgzc_flag;
+	bool ngzc_flag;
+	unsigned long long pgzc_time;				// Positive Going Zero Crossing
+	unsigned long long ngzc_time;						// Negative G     Z    C
+	bool early;					// if true, pendulum arrived at last pgzc early
 
-  };
+};
 
 // pos_array[][] ("position array") is a circular array:
 // locations [0:9][0:1] store data
@@ -71,32 +69,31 @@ struct pendulum_t
 // location [10][0] stores the location of last datum added, eg if pos_array[10][0] == 3, then the last data were written into pos_array[3][0:1]
 // location [10][1] is garbage, pos_array[][] was originally called my_array, there are still some legacy references to my_array
 
+struct admin_t {
+};
 
- struct admin_t
- {
- };
+long elec_pos(long sensor_reading);
+long mapping(long x, long in_min, long in_max, long out_min, long out_max);
+bool measure_due(struct time_tracker_t *time_track_var, long last_measure_time);
+bool motor_update_due(struct time_tracker_t *time_track_var,
+		long last_measure_time);
+long ngzc(struct pendulum_t *p_pendulum);
+long pgzc(struct pendulum_t *p_pendulum);
 
- long elec_pos(long sensor_reading);
-  long mapping(long x, long in_min, long in_max, long out_min, long out_max);
-  bool measure_due(struct time_tracker_t * time_track_var, long last_measure_time);
-  bool motor_update_due(struct time_tracker_t * time_track_var, long last_measure_time);
-  long ngzc(struct pendulum_t * p_pendulum);
-  long pgzc(struct pendulum_t * p_pendulum);
-
-  void set_neutral_position(struct pendulum_t * p_pendulum, long position);
-  void update_time(struct time_tracker_t * time_track_var, long time);
-  void update_max_min(struct pendulum_t * p_pendulum);
-  void calculate_current_vector(struct pendulum_t * p_pendulum, long position, int magnitude); // current as in Amperes, not 'present' or dried grape
-  //void apply_current_vector(struct pendulum_t * p_pendulum);                                   // current as in Amperes, not 'present' or dried grape
-  void apply_specified_current_vector(struct pendulum_t * p_pendulum, int a);
-  long desired_pgzc(long time, struct pendulum_t * p_pendulum, struct time_tracker_t * p_time_track_var); // returns the time at which the most recent pgzc should have been seen
-  long apply_neutral_offset(long reading, struct pendulum_t * p_pendulum);
-  void determine_late_early(struct pendulum_t * p_pendulum, struct time_tracker_t * p_time_track_var);
-
-
+void set_neutral_position(struct pendulum_t *p_pendulum, long position);
+void update_time(struct time_tracker_t *time_track_var, long time);
+void update_max_min(struct pendulum_t *p_pendulum);
+void calculate_current_vector(struct pendulum_t *p_pendulum, long position,
+		int magnitude); // current as in Amperes, not 'present' or dried grape
+//void apply_current_vector(struct pendulum_t * p_pendulum);                                   // current as in Amperes, not 'present' or dried grape
+void apply_specified_current_vector(struct pendulum_t *p_pendulum, int a);
+long desired_pgzc(long time, struct pendulum_t *p_pendulum,
+		struct time_tracker_t *p_time_track_var); // returns the time at which the most recent pgzc should have been seen
+long apply_neutral_offset(long reading, struct pendulum_t *p_pendulum);
+void determine_late_early(struct pendulum_t *p_pendulum,
+		struct time_tracker_t *p_time_track_var);
 
 #endif /* INC_FUNCTIONS_STRUCTS_H_ */
-
 
 //
 //  static void MX_GPIO_Init(void)
--- Z_C091KCT6_Rev_B_board_pendulum_driver/Core/Src/functions_structs.c
+++ Z_C091KCT6_Rev_B_board_pendulum_driver/Core/Src/functions_structs.c
@@ -12,13 +12,11 @@
  *      Author: andrewchisholm
  */
 
-
 #include "functions_structs.h"
 
 #include <stdlib.h>
 
 extern int pwmSin[];
-
 
 #ifndef FUNCTIONS_STRUCTS
 #define FUNCTIONS_STRUCTS
@@ -32,163 +30,169 @@ extern int pwmSin[];
    ((__CHANNEL__) == TIM_CHANNEL_2) ? ((__HANDLE__)->Instance->CCR2 = (__COMPARE__)) :\
    ((__CHANNEL__) == TIM_CHANNEL_3) ? ((__HANDLE__)->Instance->CCR3 = (__COMPARE__)) :\
   /* ((__CHANNEL__) == TIM_CHANNEL_4) ? ((__HANDLE__)->Instance->CCR4 = (__COMPARE__)) :\ */
-  /* ((__CHANNEL__) == TIM_CHANNEL_5) ? ((__HANDLE__)->Instance->CCR5 = (__COMPARE__)) :\ */
-  /* ((__HANDLE__)->Instance->CCR6 = (__COMPARE__))) */
+/* ((__CHANNEL__) == TIM_CHANNEL_5) ? ((__HANDLE__)->Instance->CCR5 = (__COMPARE__)) :\ */
+/* ((__HANDLE__)->Instance->CCR6 = (__COMPARE__))) */
 
 #endif
-long mapping(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+long mapping(long x, long in_min, long in_max, long out_min, long out_max) {
+	return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
-long apply_neutral_offset(long reading, struct pendulum_t * p_pendulum)
-{
+long apply_neutral_offset(long reading, struct pendulum_t *p_pendulum) {
 	long dummy = reading - p_pendulum->neutral_position;
-	long ret_val= 98765;
-	if( dummy > 8191)
-	{
+	long ret_val = 98765;
+	if (dummy > 8191) {
 		ret_val = dummy - 16384;
-	}
-	else if( dummy < -8192)
-	{
+	} else if (dummy < -8192) {
 		ret_val = dummy + 16384;
-	}
-	else
-	{
+	} else {
 		ret_val = dummy;
 	}
 
 	return ret_val;
 }
 
-void calculate_current_vector(struct pendulum_t * p_pendulum, long position, int magnitude)
-{
-  int dir;
-  if (magnitude <= 0){dir = 36;}
-  else dir = 12;
-  int dummyA = ((elec_pos(position) + dir) % 48); // direction = 12 or 36, dummy is the value in array pwmSin to select
-  int dummyB = (dummyA + 16) % 48;
-  int dummyC = (dummyA + 32) % 48;
+void calculate_current_vector(struct pendulum_t *p_pendulum, long position,
+		int magnitude) {
+	int dir;
+	if (magnitude <= 0) {
+		dir = 36;
+	} else
+		dir = 12;
+	int dummyA = ((elec_pos(position) + dir) % 48); // direction = 12 or 36, dummy is the value in array pwmSin to select
+	int dummyB = (dummyA + 16) % 48;
+	int dummyC = (dummyA + 32) % 48;
 
-  if (abs(magnitude) > 100){magnitude = 100;} // don't care about the sign of magnitude any more.
-  // TODO magnitude is a bad word for this job, replace with eg torque
+	if (abs(magnitude) > 100) {
+		magnitude = 100;
+	} // don't care about the sign of magnitude any more.
+	// TODO magnitude is a bad word for this job, replace with eg torque
 
-  p_pendulum->current_vector[0] = (int) ( abs(magnitude) * pwmSin[dummyA] / 100.0 );
-  p_pendulum->current_vector[1] = (int) ( abs(magnitude) * pwmSin[dummyB] / 100.0 );
-  p_pendulum->current_vector[2] = (int) ( abs(magnitude) * pwmSin[dummyC] / 100.0 );
+	p_pendulum->current_vector[0] = (int) (abs(magnitude) * pwmSin[dummyA]
+			/ 100.0);
+	p_pendulum->current_vector[1] = (int) (abs(magnitude) * pwmSin[dummyB]
+			/ 100.0);
+	p_pendulum->current_vector[2] = (int) (abs(magnitude) * pwmSin[dummyC]
+			/ 100.0);
 }
 
-
-
-
-long elec_pos(long sensor_reading)
-{
-long dummy = mapping(sensor_reading,-8192,8191, 0, 48 * 7); // 'electrical position'
-return dummy;
+long elec_pos(long sensor_reading) {
+	long dummy = mapping(sensor_reading, -8192, 8191, 0, 48 * 7); // 'electrical position'
+	return dummy;
 }
 
-bool measure_due(struct time_tracker_t * time_track_var, long last_measure_time)
-{
-  long dummy  = time_track_var->loop_time_current;
-  if (dummy - last_measure_time >= time_track_var->measurement_interval) {return true;}
-  else return false;
+bool measure_due(struct time_tracker_t *time_track_var, long last_measure_time) {
+	long dummy = time_track_var->loop_time_current;
+	if (dummy - last_measure_time >= time_track_var->measurement_interval) {
+		return true;
+	} else
+		return false;
 }
 
-void set_neutral_position(struct pendulum_t * p_pendulum, long position)
-{
-  p_pendulum->neutral_position = position;
+void set_neutral_position(struct pendulum_t *p_pendulum, long position) {
+	p_pendulum->neutral_position = position;
 }
 
-void update_time(struct time_tracker_t * time_track_var, long time)
-{
-  time_track_var->loop_time_previous = time_track_var->loop_time_current;
-  time_track_var->loop_time_current = time;
+void update_time(struct time_tracker_t *time_track_var, long time) {
+	time_track_var->loop_time_previous = time_track_var->loop_time_current;
+	time_track_var->loop_time_current = time;
 }
 
-
-long pgzc(struct pendulum_t * p_pendulum) // returns the est. time at which the pendulum crosses zero going +ve, updates pendulum struct
+long pgzc(struct pendulum_t *p_pendulum) // returns the est. time at which the pendulum crosses zero going +ve, updates pendulum struct
 {
-  if (p_pendulum->neutral_position < 0){ return -1;} // neutral position has not been measured (so all readings are meaningless)
-  long positionB = p_pendulum->pos_array[p_pendulum->pos_array[10][0]][0]; // newest position taken into account
-  long positionA = p_pendulum->pos_array[(p_pendulum->pos_array[10][0]+9)%10][0]; // oldest position taken into account
-  if (positionB >= 0 && positionA < 0 && p_pendulum->pgzc_flag == true )
-    {
-      p_pendulum->pos_reading_min = p_pendulum->pos_reading_provisional_min;
-      p_pendulum->pos_reading_provisional_min = 20123;
-      //p_pendulum->pos_reading_max = p_pendulum->pos_reading_provisional_max;
-      //p_pendulum->pos_reading_provisional_max = 0;
-      p_pendulum->amplitude = p_pendulum->pos_reading_max - p_pendulum->pos_reading_min;
-      float numerator   = positionB * p_pendulum->pos_array[(p_pendulum->pos_array[10][0]+9)%10][1] - positionA * p_pendulum->pos_array[p_pendulum->pos_array[10][0]][1];
-      float denominator = positionB - positionA;
-      long crossing_time = (long)(numerator / denominator);
-      p_pendulum->ngzc_flag = true; p_pendulum->pgzc_flag = false; //
-      p_pendulum->period    = (crossing_time - p_pendulum->pgzc_time);
-      p_pendulum->pgzc_time =  crossing_time;
-      return crossing_time;
-    }
-  else {return -2;} // benign, but no zero crossing
+	if (p_pendulum->neutral_position < 0) {
+		return -1;
+	} // neutral position has not been measured (so all readings are meaningless)
+	long positionB = p_pendulum->pos_array[p_pendulum->pos_array[10][0]][0]; // newest position taken into account
+	long positionA = p_pendulum->pos_array[(p_pendulum->pos_array[10][0] + 9)
+			% 10][0]; // oldest position taken into account
+	if (positionB >= 0 && positionA < 0 && p_pendulum->pgzc_flag == true) {
+		p_pendulum->pos_reading_min = p_pendulum->pos_reading_provisional_min;
+		p_pendulum->pos_reading_provisional_min = 20123;
+		//p_pendulum->pos_reading_max = p_pendulum->pos_reading_provisional_max;
+		//p_pendulum->pos_reading_provisional_max = 0;
+		p_pendulum->amplitude = p_pendulum->pos_reading_max
+				- p_pendulum->pos_reading_min;
+		float numerator =
+				positionB
+						* p_pendulum->pos_array[(p_pendulum->pos_array[10][0]
+								+ 9) % 10][1]
+						- positionA
+								* p_pendulum->pos_array[p_pendulum->pos_array[10][0]][1];
+		float denominator = positionB - positionA;
+		long crossing_time = (long) (numerator / denominator);
+		p_pendulum->ngzc_flag = true;
+		p_pendulum->pgzc_flag = false; //
+		p_pendulum->period = (crossing_time - p_pendulum->pgzc_time);
+		p_pendulum->pgzc_time = crossing_time;
+		return crossing_time;
+	} else {
+		return -2;
+	} // benign, but no zero crossing
 }
 
-long ngzc(struct pendulum_t * p_pendulum) // TODO does not cope with neutral position ~= 0
+long ngzc(struct pendulum_t *p_pendulum) // TODO does not cope with neutral position ~= 0
 {
-  if (p_pendulum->neutral_position < 0){ return -1;} // neutral position has not been measured (so all readings are meaningless)
-  long positionB = p_pendulum->pos_array[p_pendulum->pos_array[10][0]][0]; // newest position taken into account
-  long positionA = p_pendulum->pos_array[(p_pendulum->pos_array[10][0]+9)%10][0]; // oldest position taken into account
-  if (positionB <= 0 && positionA > 0 && p_pendulum->ngzc_flag == true) // condition for position to have crossed from +ve to -ve position
-    {
-      p_pendulum->pos_reading_max = p_pendulum->pos_reading_provisional_max;
-      p_pendulum->pos_reading_provisional_max = 0;
-      //p_pendulum->pos_reading_min = p_pendulum->pos_reading_provisional_min;
-      //p_pendulum->pos_reading_provisional_min = 20123;
-      p_pendulum->amplitude = p_pendulum->pos_reading_max - p_pendulum->pos_reading_min;
-      float numerator   = positionB * p_pendulum->pos_array[(p_pendulum->pos_array[10][0]+9)%10][1] - positionA * p_pendulum->pos_array[p_pendulum->pos_array[10][0]][1];
-      float denominator = positionB - positionA;
-      long crossing_time = (long)(numerator / denominator); // linear interpolation of crossing time
-      p_pendulum->ngzc_flag = false; p_pendulum->pgzc_flag = true; //
-      p_pendulum->ngzc_time = crossing_time;
-      return crossing_time;
-    }
-  else {return -2;} // benign, but no zero crossing
+	if (p_pendulum->neutral_position < 0) {
+		return -1;
+	} // neutral position has not been measured (so all readings are meaningless)
+	long positionB = p_pendulum->pos_array[p_pendulum->pos_array[10][0]][0]; // newest position taken into account
+	long positionA = p_pendulum->pos_array[(p_pendulum->pos_array[10][0] + 9)
+			% 10][0]; // oldest position taken into account
+	if (positionB <= 0 && positionA > 0 && p_pendulum->ngzc_flag == true) // condition for position to have crossed from +ve to -ve position
+	{
+		p_pendulum->pos_reading_max = p_pendulum->pos_reading_provisional_max;
+		p_pendulum->pos_reading_provisional_max = 0;
+		//p_pendulum->pos_reading_min = p_pendulum->pos_reading_provisional_min;
+		//p_pendulum->pos_reading_provisional_min = 20123;
+		p_pendulum->amplitude = p_pendulum->pos_reading_max
+				- p_pendulum->pos_reading_min;
+		float numerator =
+				positionB
+						* p_pendulum->pos_array[(p_pendulum->pos_array[10][0]
+								+ 9) % 10][1]
+						- positionA
+								* p_pendulum->pos_array[p_pendulum->pos_array[10][0]][1];
+		float denominator = positionB - positionA;
+		long crossing_time = (long) (numerator / denominator); // linear interpolation of crossing time
+		p_pendulum->ngzc_flag = false;
+		p_pendulum->pgzc_flag = true; //
+		p_pendulum->ngzc_time = crossing_time;
+		return crossing_time;
+	} else {
+		return -2;
+	} // benign, but no zero crossing
 }
 
-long desired_pgzc(long time, struct pendulum_t * p_pendulum, struct time_tracker_t * p_time_track_var ) // returns time of most recent desired pgzc
+long desired_pgzc(long time, struct pendulum_t *p_pendulum,
+		struct time_tracker_t *p_time_track_var) // returns time of most recent desired pgzc
 {
 	// TODO refine this to use microseconds
-	return (p_time_track_var->meta_period_start_time +
-			((time - p_time_track_var->meta_period_start_time) / p_pendulum->desired_period) * p_pendulum->desired_period);
+	return (p_time_track_var->meta_period_start_time
+			+ ((time - p_time_track_var->meta_period_start_time)
+					/ p_pendulum->desired_period) * p_pendulum->desired_period);
 }
 
-void determine_late_early(struct pendulum_t * p_pendulum, struct time_tracker_t * p_time_track_var)
-{
+void determine_late_early(struct pendulum_t *p_pendulum,
+		struct time_tracker_t *p_time_track_var) {
 	unsigned long long A = p_pendulum->pgzc_time;
 	unsigned long long D = p_time_track_var->pgzc_time_last_desired;
 	long long AlessD = A - D;
 
-	if ((AlessD) > 0)
-	{
-		if ( 2 * abs(AlessD) > (p_pendulum->desired_period ) )
-		{
+	if ((AlessD) > 0) {
+		if (2 * abs(AlessD) > (p_pendulum->desired_period)) {
 			p_pendulum->early = true;
-		}
-		else
-		{
+		} else {
 			p_pendulum->early = false;
 		}
 	}
 
-	if ((AlessD) <= 0)
-	{
-		if ( 2 * abs(AlessD) > (p_pendulum->desired_period ) )
-		{
+	if ((AlessD) <= 0) {
+		if (2 * abs(AlessD) > (p_pendulum->desired_period)) {
 			p_pendulum->early = false;
-		}
-		else
-		{
+		} else {
 			p_pendulum->early = true;
 		}
 	}
 }
-
-
-
 
